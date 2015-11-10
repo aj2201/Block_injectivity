e7b8946429cdeeacbeb6e05e970b4a7999f84878
Aygul Ibatullina <Aygul.Ibatullina@salympetroleum.ru>
2015-11-10 16:47:45 +0300

    file with physical contsants

---
+++ PVTprops.py
@@ -0,0 +1,24 @@
+class PVTprops:
+    """
+    module with constants for further calculations
+    """
+    #M = 555
+    
+    
+    #def __init__(self):
+    mu_w = 0.4
+    mu_o = 2.20
+    krw_prime = 0.30
+    kro_prime = 0.80
+    d = 500
+    rw = 0.08
+    C = 18.41
+    p_wf_I = 390
+    p_wf_P = 40
+    s_inj = -3
+    s_prod_frac = -3
+    s_prod = 0
+    M_T = 1.2
+    p_avg_D_pat = 8.30
+    p_avg_D_well = 7.55
+        
