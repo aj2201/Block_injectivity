1ed5a9e7cb8ba2bee76cb96f76a9ad7a2a10399b
Aygul Ibatullina <Aygul.Ibatullina@salympetroleum.ru>
2015-11-12 19:04:08 +0300

    bugs found

--- BlockInjIndex.py
+++ BlockInjIndex.py
@@ -5,10 +5,6 @@ import pandas as pd
 import PVTprops as pvt
 import seaborn
 
-MAX_WELLS_COUNT = 964
-MAX_MONTHS_COUNT = 240
-
-
 
 class BlockInjIndex:
     """
@@ -36,11 +32,11 @@ class BlockInjIndex:
         class initialization
         args is names of input files 
         """
-        self.__NinetyInputFile = NinetyDaysPresInputFileStr
-        self.__InjOfmFile = InjOfmFileStr
-        self.__InterpFile =  '.\input\\06 Petrophysical Evaluation results in RGTI form.csv' #[".\input\\InterpretationSVA.csv", ".\input\\InterpretationUS.csv",".\input\\InterpretationWS.csv"]
-        self.__BlockMappingFile = BlockMappingFileInput
-        self.__CellMappingFile = CellsMappingFileInput
+        self.NinetyInputFile = NinetyDaysPresInputFileStr
+        self.InjOfmFile = InjOfmFileStr
+        self.InterpFile =  '.\input\\06 Petrophysical Evaluation results in RGTI form.csv' #[".\input\\InterpretationSVA.csv", ".\input\\InterpretationUS.csv",".\input\\InterpretationWS.csv"]
+        self.BlockMappingFile = BlockMappingFileInput
+        self.CellMappingFile = CellsMappingFileInput
                 
         #self.wells = {}
         # self.start_date = "01/09/2006"
@@ -67,7 +63,7 @@ class BlockInjIndex:
         loading inj rates and bhp from txt file (ofm report):
             @name(), Date, MonthlyWaterInj.VolumeTot, MonthlyWaterInj.Days, avg_BhpInjTopPerfFaily
         """
-        df = pd.read_table(self.__InjOfmFile, ",")
+        df = pd.read_table(self.InjOfmFile, ",")
         df['Date'] = pd.to_datetime(df['Date'])
         df["InjRate"] = df["MonthlyWaterInj.VolumeTot"] / df["MonthlyWaterInj.Days"] 
         #df["Date"] = self.year_month(df["Date"]) #to avoid porblems with days (last vs first)
@@ -84,7 +80,7 @@ class BlockInjIndex:
         loading reservoir pressures from txt file (ofm report):
         # @name(), Date,  Ninetydaysplan.Pres, Ninetydaysplan.Pi, Ninetydaysplan.Qgross 
         """
-        df = pd.read_table(self.__NinetyInputFile, ",")
+        df = pd.read_table(self.NinetyInputFile, ",")
         df['Date'] = pd.to_datetime(df['Date'])
         df["Pres"] = df["Ninetydaysplan.Pres"]
         df["Pi"] = df["Ninetydaysplan.Pi"] 
@@ -110,7 +106,7 @@ class BlockInjIndex:
         
         """
         
-        filename = self.__InterpFile  # for each filed (sva, ws, us)
+        filename = self.InterpFile  # for each filed (sva, ws, us)
         df = pd.read_table(filename, ";")#,error_bad_lines=False)
         df['Perforation'] = df['If exist perforation'] =='perforated'
         
@@ -124,7 +120,7 @@ class BlockInjIndex:
         loading blocks mapping
         Cell     Well       WAF, and additionally block column
         """
-        input_table = pd.read_table(self.__BlockMappingFile, ";")                                       #reading the file 
+        input_table = pd.read_table(self.BlockMappingFile, ";")                                       #reading the file 
         #input_table["cell"]
         input_table["block"] = map(lambda s: s[:s.find('-', 4)], input_table["Cell"])                 #making new column with block name
         
@@ -133,12 +129,12 @@ class BlockInjIndex:
         self.blocks_list = list(set(input_table["block"]))
         self.blocks_dict = {a : list(set(input_table[input_table["block"]==a]["Well"])) for a in  self.blocks_list}                        # dictionary block name is key, and for each keycorresponds list of its wells
         #adding fields as bocks
-        t.blocks_dict["US"] = filter(lambda t: t[:2]=="US",  t.cells_list )
-        t.blocks_dict["SVA"] = filter(lambda t: t[:2]=="SV",  t.cells_list )
-        t.blocks_dict["WS"] = filter(lambda t: t[:2]=="WS",  t.cells_list )
-        t.blocks_list.append("US")
-        t.blocks_list.append("WS")
-        t.blocks_list.append("SVA")
+        self.blocks_dict["US"] = filter(lambda x: x[:2]=="US",  self.cells_list )
+        self.blocks_dict["SVA"] = filter(lambda x: x[:2]=="SV",  self.cells_list )
+        self.blocks_dict["WS"] = filter(lambda x: x[:2]=="WS",  self.cells_list )
+        self.blocks_list.append("US")
+        self.blocks_list.append("WS")
+        self.blocks_list.append("SVA")
         
     
     def cells_mapping(self):
@@ -146,7 +142,7 @@ class BlockInjIndex:
         loading cells mapping
         :Injector;Koeff;Producer
         """
-        df = pd.read_table(self.__CellMappingFile, ";")
+        df = pd.read_table(self.CellMappingFile, ";")
         self.cells_list = list(set(df["Injector"])) #list(pd.pivot_table(df, rows=["Injector"], values=["Koeff"]).axes[0])
         #self.WAF_table = pd.pivot_table(df, rows=["Injector", "Producer"], values=["Koeff"])
         self.cells_dict = { a: list(set(df[df["Injector"]==a]["Producer"]) - set(self.cells_list)) for a in self.cells_list}
@@ -165,37 +161,38 @@ class BlockInjIndex:
         """
         loading data
         """
-        if __debug__:
+        if False: #__debug__:
             print "loading data from files:"
-            print "\t*", self.__NinetyInputFile
-            print "\t*",self.__InjOfmFile
-            print "\t*",self.__InterpFile
-            print "\t*",self.__BlockMappingFile
-            print "\t*",self.__CellMappingFile
+            print "\t*", self.NinetyInputFile
+            print "\t*",self.InjOfmFile
+            print "\t*",self.InterpFile
+            print "\t*",self.BlockMappingFile
+            print "\t*",self.CellMappingFile
         self.load_inj_rates_PT()
         self.load_90dp_PT()
         self.load_kh()
-        self.blocks_mapping()
         self.cells_mapping()
-        if __debug__: print "data loaded"   
+        self.blocks_mapping()
+        
+        if True: print "data loaded"   
     
     
         
         
     
-    def main_calc(self, blocks_list_for_calc=None):
+    def injectivity_skin_calc(self, blocks_list_for_calc=None):
         #self.load_data()
-        if __debug__: print "calculating..."
+        #if __debug__: print "calculating..."
         #calculating injectors pressure as average of neighbor producers 90dp pressure
         inj_Pres_table = pd.DataFrame()
         for injector in self.cells_list:  #cell contain injector and surrounding producers
-           neighbors = t.cells_dict.get(injector)  # getting neighbors list
+           neighbors = self.cells_dict.get(injector)  # getting neighbors list
            #neighbors = neighbors[neighbors!=injector]
-           neighbors_Pres = pd.DataFrame() #[t.pres_table[i] for i in neighbors and ]) #getting pressures of neighbors
+           neighbors_Pres = pd.DataFrame() #[self.pres_table[i] for i in neighbors and ]) #getting pressures of neighbors
            some_list = []
            for i in neighbors:
-               if str(type(t.pres_table.get(i))) != "<type 'NoneType'>":
-                   some_list.append( t.pres_table[i])
+               if str(type(self.pres_table.get(i))) != "<type 'NoneType'>":
+                   some_list.append( self.pres_table[i])
            neighbors_Pres = pd.DataFrame(some_list)
            injector_Pres = neighbors_Pres.mean(skipna=True, axis=0)  # averaging of non NaN values, i hope that its like to numpy.nanmean (v1.11)
            inj_Pres_table[injector] = injector_Pres  # pressure of injector calculated as average of neighbor producers 90dp pressure
@@ -203,12 +200,12 @@ class BlockInjIndex:
         # beta_coef calculating =  -(p_avg_D_pat-LN(2)) where p_avg_D_pat = LN(d/rw)-0.443 = 8.30
         beta_coef = -(pvt.PVTprops.p_avg_D_pat - np.log(2))
         #alpha for each well =  preforated kh*krw/mu_w/C
-        alpha_table = t.kh_table * pvt.PVTprops.krw_prime / pvt.PVTprops.mu_w / pvt.PVTprops.C
+        alpha_table = self.kh_table * pvt.PVTprops.krw_prime / pvt.PVTprops.mu_w / pvt.PVTprops.C
         alpha_table.columns=['alpha']
         # zero values assign as NAN to avoid misstakes in averaging
         #alpha_table[alpha_table['alpha']==0] = np.nan
         # table = (BHP - Pres) / InjRate
-        table = (t.bhp_table.T['avg_BhpInjTopPerfFaily']-inj_Pres_table.T['Pres'] ) / t.inj_table.T['InjRate']
+        table = (self.bhp_table.T['avg_BhpInjTopPerfFaily']-inj_Pres_table.T['Pres'] ) / self.inj_table.T['InjRate']
         
         #adding to alpha table NAN values for wells which not presented in RGTI tables 
         #                               (index of alpha_table equal to index of kh_table)
@@ -219,9 +216,9 @@ class BlockInjIndex:
         # inj_index = table * alpha + beta
         self.inj_index = pd.DataFrame(table.values*alpha_df.values, index=table.index, columns=table.columns) + beta_coef
         # deleting from blocks_dict all producers, since for blocks injectivity calculating only injectors requried
-        blocks_inj_dict = {a: list(set(t.blocks_dict[a]) & set(t.cells_list)) for a in t.blocks_list}
+        blocks_inj_dict = {a: list(set(self.blocks_dict[a]) & set(self.cells_list)) for a in self.blocks_list}
         # eqclude cells from blocks list 
-        big_blocks = list(set(t.blocks_list) - set(t.cells_list))
+        big_blocks = list(set(self.blocks_list) - set(self.cells_list))
             
         #sorting list for beauty
         big_blocks.sort()
@@ -229,7 +226,7 @@ class BlockInjIndex:
             if len(blocks_inj_dict[block]) > 0:
                 self.block_inj_index[block] = pd.DataFrame([self.inj_index.T[injector] for injector in blocks_inj_dict[block]]).median(axis=0, skipna=True)
             else: 
-                if __debug__: print block, "is empty [injectors didn't found]"
+                #if __debug__: print block, "is empty [injectors didn't found]"
                 self.block_inj_index[block] =  np.nan
         self.block_inj_index[self.block_inj_index>1000]=np.nan
         self.block_inj_index.sort_index
@@ -261,15 +258,17 @@ class BlockInjIndex:
         return (values * weights).groupby(by).sum() / weights.groupby(by).sum()
 
 
-  
+"""  
 if __name__ == "__main__":
     #if __debug__:
     #    print "Debug"
     t = BlockInjIndex()
     t.load_data()
-    t.main_calc()
+    t.injectivity_skin_calc()
+    
     SVA_blocks = filter(lambda t: t[:2]=="SV",t.block_inj_index.columns)
     WS_blocks = filter(lambda t: t[:2]=="WS", t.block_inj_index.columns)
     US_blocks = filter(lambda t: t[:2]=="US", t.block_inj_index.columns)
     t.plot_list(WS_blocks)
     #grouped_weighted_avg(values=df.wt, weights=df.value, by=df.index)
+"""
