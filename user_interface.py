7c76725d34ff59cdc37689b9011b109b8dc16a60
Aygul Ibatullina <Aygul.Ibatullina@salympetroleum.ru>
2015-11-12 20:28:12 +0300

    check_boxes added

--- user_interface.py
+++ user_interface.py
@@ -6,9 +6,9 @@ Created on Fri Nov 06 17:18:52 2015
 """
 from PyQt4 import QtCore, QtGui
 import sys
-from BlockInjIndex import *
-from Ui_AboutWidget import *
-from Ui_MainWindow import *
+from BlockInjIndex import BlockInjIndex
+from Ui_AboutWidget import Ui_AboutWidget
+from Ui_MainWindow import Ui_MainWindow
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -35,6 +35,7 @@ class user_interface(Ui_MainWindow):
         self.setup_file_open_dialogs()
         #QtCore.QObject.connect(self.start_time_edit, QtCore.SIGNAL(_fromUtf8("dateChanged(QDate)")), self.menubar.raise_)
         QtCore.QObject.connect(self.load_input_files_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.load_files)
+        QtCore.QObject.connect(self.load_blocks_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.load_blocks_list)
         QtCore.QObject.connect(self.actionClear_Data, QtCore.SIGNAL(_fromUtf8("activated()")), self.clear)
         QtCore.QObject.connect(self.actionAbout, QtCore.SIGNAL(_fromUtf8("activated()")), self.about_dialog)
         filename = "./input/!readMe.txt"
@@ -42,8 +43,8 @@ class user_interface(Ui_MainWindow):
         data = file.read()
         self.input_file_help_text.setText(data)
         self.tabWidget.setCurrentIndex(0)
-        
-        
+        QtCore.QObject.connect(self.load_input_files_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.load_files)
+        QtCore.QObject.connect(self.select_all_check_box, QtCore.SIGNAL(_fromUtf8("stateChanged(int)")),self.selectAll)
     
     def setup_file_open_dialogs(self):
         self.Ndp_file_edit.setText(self.bii.NinetyInputFile)
@@ -55,19 +56,25 @@ class user_interface(Ui_MainWindow):
         QtCore.QObject.connect(self.ofm_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.ofm_file_dialog)
         QtCore.QObject.connect(self.rgti_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.rgti_file_dialog)
         QtCore.QObject.connect(self.cells_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.cells_file_dialog)
-        QtCore.QObject.connect(self.blocks_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.blocks_file_dialog)
+        QtCore.QObject.connect(self.blocks_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.blocks_file_dialog)        
         
     def __init__(self):
         self.bii = BlockInjIndex()
         #self.main = Ui_MainWindow()
         self.about_w = Ui_AboutWidget()
         self.inputs_load = 0
+        self.blocks_list_for_analysis = []
         #self.setupUi2()
+        
+    def load_blocks_list(self):
+        self.blocks_list_for_analysis = []
+        
+        return
         
     def clear(self):
         self.__init__()
         self.setup_file_open_dialogs()
-        gc.collect()
+        #gc.collect()
         self.statusBar().showMessage('Data cleared')
     
     def load_files(self):
@@ -79,8 +86,8 @@ class user_interface(Ui_MainWindow):
             (not QtCore.QFile.exists(self.blocks_file_edit.text())) | \
             (not QtCore.QFile.exists(self.cells_file_edit.text())) :
             QtGui.QMessageBox.about(None, "Message", "Data loaded\n one of input files doesn't exists")
-            print "bad"
-            return false
+            #print "bad"
+            return
         
         self.bii.NinetyInputFile = self.Ndp_file_edit.text()
         self.bii.InjOfmFile = self.ofm_file_edit.text()
@@ -89,20 +96,30 @@ class user_interface(Ui_MainWindow):
         self.bii.CellMappingFile = self.cells_file_edit.text()
         print "smthg"
         self.bii.load_data()
-        QtGui.QMessageBox.about(None, "Message", "Data loaded")
-        self.statusBar().showMessage('Data loaded')
+        #QtGui.QMessageBox.about(None, "Message", "Data loaded")
+        #self.statusBar().showMessage('Data loaded')
         self.create_check_boxes()
         
     def create_check_boxes(self):
-        for n in range(10):                   
-            item = QtGui.QStandardItem('Item %s' % randint(1, 100))
-            check = QtCore.Qt.Checked if randint(0, 1) == 1 else Qt.Unchecked
+        """create dynamycally list of checkboxes from blocks_list"""
+        model = QtGui.QStandardItemModel()
+        check = QtCore.Qt.Checked
+        for n in self.bii.blocks_list :                   
+            item = QtGui.QStandardItem(n)
             item.setCheckState(check)
             item.setCheckable(True)
             model.appendRow(item)
+        self.listView.setModel(model)
+        
 
-        return 0
-    
+    def selectAll(self, state=QtCore.Qt.Checked):
+        """Select All layers loaded inside the listView"""
+        model = self.listView.model()
+        for index in range(model.rowCount()):
+            item = model.item(index)
+            if item.isCheckable():
+                item.setCheckState(state)
+            
     def about_dialog(self):
         about = QtGui.QWidget()
         self.about_w.setupUi(about)
