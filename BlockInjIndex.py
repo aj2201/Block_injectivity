1eab13a727a90bb55e3328348263b07b79dbd3a2
Aygul Ibatullina <Aygul.Ibatullina@salympetroleum.ru>
2015-11-17 12:58:16 +0300

    added weighted average for skin calc, comments edited, wafs for field added

--- BlockInjIndex.py
+++ BlockInjIndex.py
@@ -13,8 +13,8 @@ class BlockInjIndex:
         pres_table - reservoir pressure estimates pres_table[well][date*]
         bhp_table -BHPs of injectors
         inj_table - injection rates [well][date*]
-        kh_table - 
-        
+        kh_table - perforated kh for each well
+                
         
     """
 
@@ -74,6 +74,7 @@ class BlockInjIndex:
         else:
             self.inj_table = pd.pivot_table(df, values=["InjRate"], columns=["Date"], index=["@name()"]).T
             self.bhp_table = pd.pivot_table(df, values=["avg_BhpInjTopPerfFaily"], columns=["Date"], index=["@name()"]).T
+            
                     
     def load_90dp_PT(self):
         """
@@ -93,50 +94,51 @@ class BlockInjIndex:
     def load_kh(self):
         """
         loading petrophisical data
-        Data file header is
-        Field;Well;Layer;Top MD [ m ];Bottom MD [ m ];Thickness MD [ m ];Top TVD [ m ];
-        Bottom TVD [ m ];Thickness TVD [ m ];Rt;Porosity [ % ];Liquid permeability [ MilliDarcy ];
-        Oil Permeability [ MilliDarcy ];Water Permeability [ MilliDarcy ];So_Log;So_Shf;Fluid;Perforation
-        
-        Mayak RGTI:
+        Data file header is(Mayak RGTI):
         UWI;UWBI;Well Name;Wellbore Name;Lithostratigraphic Unit;Sublayer Top MD;Sublayer Bottom Md ;
         Sublayer Height MD;Sublayer Top TVD SS;Sub Layer Top TVD;Sublayer Bottom TVD SS;Sub Layer Bottom TVD;
         Sublayer Height TVD;Rt;Porosity;Brine Permeability;Water Permeability;Oil Permeability;
         Oil Saturation;Saturation;If exist perforation;;
-        
         """
-        
         filename = self.InterpFile  # for each filed (sva, ws, us)
         df = pd.read_table(filename, ";")#,error_bad_lines=False)
         df['Perforation'] = df['If exist perforation'] =='perforated'
-        
         df['kh'] = df['Perforation']*df['Brine Permeability']*df['Sublayer Height TVD']  # add a column with perforated kh of  interval
         self.kh_table = pd.pivot_table(df, values=['kh'], index=['Well Name'], aggfunc=np.sum)
-        
-           
         
     def blocks_mapping(self, block_name = None):
         """
         loading blocks mapping
         Cell     Well       WAF, and additionally block column
         """
-        input_table = pd.read_table(self.BlockMappingFile, ";")                                       #reading the file 
-        #input_table["cell"]
-        input_table["block"] = map(lambda s: s[:s.find('-', 4)], input_table["Cell"])                 #making new column with block name
-        
-        self.WAF_table_blocks = pd.pivot_table(input_table, values=['WAF'], aggfunc=np.max, index=['block', 'Well'])  # creating pivot table to extract blocks with its wells, 
-                                                                                           #this table later could be used to estract WAFs       
+        #reading the file 
+        input_table = pd.read_table(self.BlockMappingFile, ";")                                       
+         #making new column with block name
+        input_table["block"] = map(lambda s: s[:s.find('-', 4)], input_table["Cell"])                
+        wells = list(set(input_table["Well"].values))
+        wells = map(str, wells[1:])
+        df = pd.DataFrame()
+        df["Well"] = wells
+        df ["WAF"] = 1
+        df ["block"] = map(lambda s: s[:s.find('-')], df["Well"])
+        df["Cell"] = df ["block"] 
+        input_table = input_table.append(df)
+        # creating pivot table to extract blocks with its wells, 
+        #this table later could be used to estract WAFs       
+        self.WAF_table_blocks = pd.pivot_table(input_table, values=['WAF'], aggfunc=np.max, index=['block', 'Well'])                                                                                             
         self.blocks_list = list(set(input_table["block"]))
-        self.blocks_dict = {a : list(set(input_table[input_table["block"]==a]["Well"])) for a in  self.blocks_list}                        # dictionary block name is key, and for each keycorresponds list of its wells
+        # dictionary block name is key, and for each keycorresponds list of its wells
+        self.blocks_dict = {a : list(set(input_table[input_table["block"]==a]["Well"])) for a in  self.blocks_list}                        
         #adding fields as bocks
-        self.blocks_dict["US"] = filter(lambda x: x[:2]=="US",  self.cells_list )
-        self.blocks_dict["SVA"] = filter(lambda x: x[:2]=="SV",  self.cells_list )
-        self.blocks_dict["WS"] = filter(lambda x: x[:2]=="WS",  self.cells_list )
-        self.blocks_list.append("US")
-        self.blocks_list.append("WS")
-        self.blocks_list.append("SVA")
-        self.blocks_list.sort()
-        
+        #TODO: aweful method! shoul be changed
+#        self.blocks_dict["US"] = filter(lambda x: x[:2]=="US",  self.cells_list )
+#        self.blocks_dict["SVA"] = filter(lambda x: x[:2]=="SV",  self.cells_list )
+#        self.blocks_dict["WS"] = filter(lambda x: x[:2]=="WS",  self.cells_list )
+#        self.blocks_list.append("US")
+#        self.blocks_list.append("WS")
+#        self.blocks_list.append("SVA")
+        #TODO: adding WAFs for fields
+        self.blocks_list.sort()    
     
     def cells_mapping(self):
         """
@@ -150,7 +152,21 @@ class BlockInjIndex:
         self.koeff_for_cells = pd.pivot_table(df, values=["Koeff"], index=["Injector", "Producer"])
         
 
-        
+    def nominal_prod_inj_ratio_calc(self):
+        """
+        nominal p/i ratio 
+        is ratio of producers count to injectors count
+        """
+        wafs = self.WAF_table_blocks.copy()
+        wafs.reset_index(inplace=True)
+        wafs["isInj"] = [ a in set(self.cells_list) for a in wafs["Well"].values]
+        wafs["injectors"] = wafs["WAF"]* (1 - wafs["isInjector"])
+        wafs["producers"] = wafs["WAF"]* wafs["isInjector"]
+        e = wafs.groupby("block").sum()
+        e["P/I"] = e["producers"]/e["injectors"]
+        self.prod_inj_ratio = e["P/I"]
+
+    
     def calc_pattern_pres(self):
         """
         for injectors (which are in cells_list)
@@ -160,31 +176,19 @@ class BlockInjIndex:
     
     def load_data(self):
         """
-        loading data
+        loading input data
         """
-        if False:
-            print "loading data from files:"
-            print "\t*", self.NinetyInputFile
-            print "\t*",self.InjOfmFile
-            print "\t*",self.InterpFile
-            print "\t*",self.BlockMappingFile
-            print "\t*",self.CellMappingFile
         self.load_inj_rates_PT()
         self.load_90dp_PT()
         self.load_kh()
         self.cells_mapping()
-        self.blocks_mapping()
-        
-        #if False: 
-        if False: print "data loaded bii"   
+        self.blocks_mapping()        
     
-    
-        
-        
-    
-    def injectivity_skin_calc(self, blocks_list_for_calc=None):
+    def injectivity_skin_calc(self):
+        """
+        calculating injectors skin (from steady state flow eq for single well)
+        """
         #self.load_data()
-        if False: print "calculating..."
         #calculating injectors pressure as average of neighbor producers 90dp pressure
         inj_Pres_table = pd.DataFrame()
         for injector in self.cells_list:  #cell contain injector and surrounding producers
@@ -218,21 +222,34 @@ class BlockInjIndex:
         # inj_index = table * alpha + beta
         self.inj_index = pd.DataFrame(table.values*alpha_df.values, index=table.index, columns=table.columns) + beta_coef
         # deleting from blocks_dict all producers, since for blocks injectivity calculating only injectors requried
-        blocks_inj_dict = {a: list(set(self.blocks_dict[a]) & set(self.cells_list)) for a in self.blocks_list}
+    
+    def block_inj_skin_calc(self, blocks_list_for_calc=None):
+        """
+        calcualting block injectivity skin
+        as rate - weughted average of injectors skin
+        """
+        self.injectivity_skin_calc()
+        blocks_inj_dict = {a: list(set(self.blocks_dict[a]) & set(self.cells_list) & set(self.inj_table.T.index)) for a in self.blocks_list}
         # eqclude cells from blocks list 
         if blocks_list_for_calc==None:
             blocks_list_for_calc = list(set(self.blocks_list) - set(self.cells_list))
-            
         #sorting list for beauty
         blocks_list_for_calc.sort()
         for block in blocks_list_for_calc:
             if len(blocks_inj_dict[block]) > 0:
-                self.block_inj_index[block] = pd.DataFrame([self.inj_index.T[injector] for injector in blocks_inj_dict[block]]).median(axis=0, skipna=True)
+                #TODO: weight average
+                #block_injectors_skins = pd.DataFrame([self.inj_index.T[injector] for injector in blocks_inj_dict[block]])
+                injectors = blocks_inj_dict[block]
+                block_inj_rates = self.inj_table[injectors]
+                block_inj_skins = self.inj_index.T[injectors]
+                #assumed that there arent injectors with WAF<1
+                self.block_inj_index[block] = (block_inj_skins*block_inj_rates).T.sum() / block_inj_rates.T.sum()
             else: 
                 #if __debug__: print block, "is empty [injectors didn't found]"
                 self.block_inj_index[block] =  np.nan
-        self.block_inj_index[self.block_inj_index>1000]=np.nan
+        #self.block_inj_index[self.block_inj_index>1000]=np.nan
         self.block_inj_index.sort_index
+        self.block_inj_index.index = self.block_inj_index.index.droplevel()
         #the results of calc is self.block_inj_index
     
     def plot_list(self, blocks_list):
@@ -255,23 +272,17 @@ class BlockInjIndex:
         df.plot()
     
     def save_as_csv(self, file_name="out.csv"):
-        self.block_inj_index.T.to_csv(file_name,sep=';')
-        
-    def grouped_weighted_avg(self, values, weights, by):
-        return (values * weights).groupby(by).sum() / weights.groupby(by).sum()
+        self.block_inj_index.T.to_csv(file_name,sep=';')      
 
-
-"""  
 if __name__ == "__main__":
     #if __debug__:
     #    print "Debug"
     t = BlockInjIndex()
     t.load_data()
-    t.injectivity_skin_calc()
+    t.block_inj_skin_calc()
     
     SVA_blocks = filter(lambda t: t[:2]=="SV",t.block_inj_index.columns)
     WS_blocks = filter(lambda t: t[:2]=="WS", t.block_inj_index.columns)
     US_blocks = filter(lambda t: t[:2]=="US", t.block_inj_index.columns)
-    t.plot_list(SVA_blocks)
-    #grouped_weighted_avg(values=df.wt, weights=df.value, by=df.index)
-"""
+    t.plot_list(["SVA"])
+    
