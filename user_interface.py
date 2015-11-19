91530011d4361fa251c380c45bc1d8f9a271b6c1
Aygul Ibatullina <Aygul.Ibatullina@salympetroleum.ru>
2015-11-19 20:35:14 +0300

    1) bii calc -> p/i ratio calc module > 2) map(str, *) because of porblems of pandas with Qstring > 3)new_plot - to plot 3 series on each subplot > 4) calculating moved to load_input_files - to perform only one time per load

--- user_interface.py
+++ user_interface.py
@@ -3,13 +3,18 @@
 Created on Fri Nov 06 17:18:52 2015
 
 @author: Aygul.Ibatullina
+!For exe compiling pyinsatller is used (version 3.1.dev0)
+
 """
-from PyQt4 import QtCore
-from PyQt4 import  QtGui
 import sys
 import numpy as np
+import pandas as pd
+
+from PyQt4 import QtCore
+from PyQt4 import  QtGui
 from matplotlib.backends.backend_qt4 import NavigationToolbar2QT as NavigationToolbar
-from BlockInjIndex import BlockInjIndex
+
+from ProdInjRatioCalc import ProdInjRatioCalc
 from Ui_AboutWidget import Ui_AboutWidget
 from Ui_MainWindow import Ui_MainWindow
 
@@ -28,13 +33,10 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
         
-        
-        
-
   
-class user_interface(Ui_MainWindow):
+class UserInterface(Ui_MainWindow):
     def __init__(self):
-        self.bii = BlockInjIndex()
+        self.pir_calc = ProdInjRatioCalc()
         #self.main = Ui_MainWindow()
         self.about_w = Ui_AboutWidget()
         self.inputs_load = 0
@@ -55,7 +57,6 @@ class user_interface(Ui_MainWindow):
         data = file.read()
         self.input_file_help_text.setText(data)
         self.tabWidget.setCurrentIndex(0)
-        self.toolBox.setCurrentIndex(1)
         self.navi_toolbar = NavigationToolbar(self.matplotlibwidget, self.OutputPlots)
         self.verticalLayout_6.addChildWidget(self.navi_toolbar)
         self.object.statusBar().showMessage('Ready')
@@ -71,16 +72,16 @@ class user_interface(Ui_MainWindow):
         QtCore.QObject.connect(self.load_input_files_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.load_input_files)
         QtCore.QObject.connect(self.select_all_check_box, QtCore.SIGNAL(_fromUtf8("stateChanged(int)")),self.selectAll)
         QtCore.QObject.connect(self.plots_select_all, QtCore.SIGNAL(_fromUtf8("stateChanged(int)")),self.plot_select_all_function)
-        QtCore.QObject.connect(self.plot_update_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.plot)
+        QtCore.QObject.connect(self.plot_update_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.plot_new)
         QtCore.QObject.connect(self.clear_plot_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.clear_plot)
         QtCore.QObject.connect(self.big_blocks_checkBox, QtCore.SIGNAL(_fromUtf8("stateChanged(int)")),self.big_blocks_display)
     
     def setup_file_open_dialogs(self):
-        self.Ndp_file_edit.setText(self.bii.NinetyInputFile)
-        self.ofm_file_edit.setText(self.bii.InjOfmFile)
-        self.rgti_file_edit.setText(self.bii.InterpFile)
-        self.blocks_file_edit.setText(self.bii.BlockMappingFile)
-        self.cells_file_edit.setText(self.bii.CellMappingFile)
+        self.Ndp_file_edit.setText(self.pir_calc.NinetyInputFile)
+        self.ofm_file_edit.setText(self.pir_calc.InjOfmFile)
+        self.rgti_file_edit.setText(self.pir_calc.InterpFile)
+        self.blocks_file_edit.setText(self.pir_calc.BlockMappingFile)
+        self.cells_file_edit.setText(self.pir_calc.CellMappingFile)
         QtCore.QObject.connect(self.Ndp_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.Ndp_file_dialog)
         QtCore.QObject.connect(self.ofm_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.ofm_file_dialog)
         QtCore.QObject.connect(self.rgti_file_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.rgti_file_dialog)
@@ -91,6 +92,8 @@ class user_interface(Ui_MainWindow):
     def clear_plot(self):
         self.object.statusBar().showMessage('Plots area cleared')
         self.matplotlibwidget.figure.clear()
+        #df = pd.DataFrame([0,0])
+        #df.plot(ax=self.matplotlibwidget.axes)
         self.matplotlibwidget.draw()
         #self.matplotlibwidget.update()
         
@@ -98,27 +101,77 @@ class user_interface(Ui_MainWindow):
     def plot(self):
         self.matplotlibwidget.axes.clear()
         the_list = []
+        #extract list to plot from form
         model = self.listView_2.model()
         for index in range(model.rowCount()):
             item = model.item(index)
             if item.checkState():
                 the_list.append(item.text())
-        
+        #to avoid problems with interacting of pandas as qtstring
+        the_list = map(str, the_list)
         if len(the_list)>0:
-            df = self.bii.block_inj_skin_table[the_list]
-        else: 
-            return
-        
-        plots_number = float(len(df.columns))
+            df_iskin = self.pir_calc.block_inj_skin_table[the_list]
+            #df_pskin = self.pir_calc.block_prod_skin_table[the_list]
+            #df_pi_ratio = = self.pir_calc.p[the_list]
+        else: return
+        #to calculate how plot subplots
+        plots_number = float(len(df_iskin.columns))
         v = int(np.ceil(plots_number ** 0.5))
         h = int(np.ceil(plots_number / v ))
         if plots_number < 4:
             v = int(np.min([3., plots_number]))
             h = 1
-        df.plot(subplots=True, layout=(v,h), ax=self.matplotlibwidget.axes)
+        if plots_number > 1:
+            df_iskin.plot(subplots=True, layout=(v,h), ax=self.matplotlibwidget.axes)
+        else:
+            #df_iskin.plot(ax=self.matplotlibwidget.axes)
+            #self.matplotlibwidget.figure.axes
+            print type(df_iskin)
+            pd.DataFrame(df_iskin).plot(ax=self.matplotlibwidget.axes)
+            #self.matplotlibwidget.axes.plot(df_iskin.index, df_iskin.values)
+            
         self.matplotlibwidget.draw()
         self.object.statusBar().showMessage('Plots ')
         
+        
+    def plot_new(self):
+        self.clear_plot()
+        i_skin_plot_flag = (self.checkBox_inj_skin_plot.checkState() ==QtCore.Qt.Checked)
+        p_skin_plot_flag = (self.checkBox_prod_skin_plot.checkState() ==QtCore.Qt.Checked)
+        pir_plot_flag = (self.checkBox_pi_ratio_plot.checkState() ==QtCore.Qt.Checked)
+        the_list = []
+        #extract list from listView2 to plot 
+        model = self.listView_2.model()
+        for index in range(model.rowCount()):
+            item = model.item(index)
+            if item.checkState():
+                the_list.append(item.text())
+        #to avoid problems with interacting of pandas and qtstring
+        the_list = map(str, the_list)
+        #to calc layot of subplots
+        plots_number = float(len(the_list))
+        v = int(np.ceil(plots_number ** 0.5))
+        h = int(np.ceil(plots_number / v ))
+        if plots_number < 4:
+            v = int(np.min([3., plots_number]))
+            h = 1
+        i=1
+        first_axes = self.matplotlibwidget.figure.add_subplot(v,h,i)
+        for block in the_list:
+            #plotting each block on separate subplot
+            df_iskin = self.pir_calc.block_inj_skin_table[block]
+            df_pskin = self.pir_calc.block_prod_skin_table[block]
+            df_pir = self.pir_calc.prod_inj_ratio_table[block]
+            df_iskin.name = block+" skin(i)"
+            df_pskin.name = block+" skin(p)"
+            df_pir.name = block+" P/I"
+            sub_axes = self.matplotlibwidget.figure.add_subplot(v,h,i, sharey=first_axes, sharex=first_axes)
+            if i_skin_plot_flag: pd.DataFrame(df_iskin).plot(ax=sub_axes)
+            if p_skin_plot_flag: pd.DataFrame(df_pskin).plot(ax=sub_axes)
+            if pir_plot_flag: pd.DataFrame(df_pir).plot(ax=sub_axes)
+            i = i + 1
+        self.matplotlibwidget.figure.tight_layout(pad=0.08, h_pad=0.2, w_pad=0.2)
+        self.matplotlibwidget.draw()
         
     def add_plot_list(self):
         """create dynamycally list of checkboxes from blocks_list_for_analysis"""
@@ -142,17 +195,18 @@ class user_interface(Ui_MainWindow):
     
     def load_blocks_list(self):
         self.object.statusBar().showMessage('blocks list loading')
+        self.listView_2.setModel(QtGui.QStandardItemModel())
         the_list = []
         model = self.listView.model()
         for index in range(model.rowCount()):
             item = model.item(index)
             if item.checkState():
                 the_list.append(item.text())
-        self.blocks_list_for_analysis = the_list
-        self.object.statusBar().showMessage('calculating parameters')
-        self.bii.block_inj_skin_calc(self.blocks_list_for_analysis)
-        self.add_plot_list()
+        self.blocks_list_for_analysis = map(str,the_list)
+        #self.pir_calc.pi_ratio_calc(self.blocks_list_for_analysis)
         self.object.statusBar().showMessage('blocks list loaded')
+        self.add_plot_list()
+        self.object.statusBar().showMessage('blocks list loaded.')
              
     def clear(self):
         #TODO: clear all tabs
@@ -177,14 +231,15 @@ class user_interface(Ui_MainWindow):
             self.object.statusBar().showMessage('Data not loaded one of input files doesn\'t exists')
             return
         
-        self.bii.NinetyInputFile = self.Ndp_file_edit.text()
-        self.bii.InjOfmFile = self.ofm_file_edit.text()
-        self.bii.InterpFile = self.rgti_file_edit.text()
-        self.bii.BlockMappingFile = self.blocks_file_edit.text()
-        self.bii.CellMappingFile = self.cells_file_edit.text()
-        self.bii.load_data()
-        self.create_check_boxes()
-        self.object.statusBar().showMessage('Data loaded')
+        self.pir_calc.NinetyInputFile = str(self.Ndp_file_edit.text())
+        self.pir_calc.InjOfmFile = str(self.ofm_file_edit.text())
+        self.pir_calc.InterpFile = str(self.rgti_file_edit.text())
+        self.pir_calc.BlockMappingFile = str(self.blocks_file_edit.text())
+        self.pir_calc.CellMappingFile = str(self.cells_file_edit.text())
+        self.pir_calc.load_data()
+        self.create_check_boxes()           
+        self.pir_calc.pi_ratio_calc()
+        self.object.statusBar().showMessage('Data loaded.')
      
     def big_blocks_display(self, state=QtCore.Qt.Checked):
         self.create_check_boxes(state)
@@ -194,9 +249,9 @@ class user_interface(Ui_MainWindow):
         model = QtGui.QStandardItemModel()
         check = QtCore.Qt.Unchecked
         if state==QtCore.Qt.Checked:
-            list_for_display = list(set(self.bii.blocks_list) - set(self.bii.cells_list))
+            list_for_display = list(set(self.pir_calc.blocks_list) - set(self.pir_calc.cells_list))
         else:
-            list_for_display = self.bii.blocks_list
+            list_for_display = self.pir_calc.blocks_list
         list_for_display.sort()
         for n in list_for_display:                   
             item = QtGui.QStandardItem(n)
@@ -247,7 +302,7 @@ class user_interface(Ui_MainWindow):
 if __name__=="__main__":
     app = QtGui.QApplication(sys.argv)
     form = QtGui.QMainWindow()
-    mwin = user_interface()
+    mwin = UserInterface()
     mwin.setupUi2(form)
     form.show()
     sys.exit(app.exec_())
