76c8fb811f8a02c2845d0cc4809669b30f89dab8
Aygul Ibatullina <Ajgulib@gmail.com>
2017-07-03 17:43:29 +0300

    new function: block_skin_calc_kh_av. for calculating skins based on block average kh, and use it to P/I ratio calculation

--- ProdInjRatioCalc.py
+++ ProdInjRatioCalc.py
@@ -59,6 +59,58 @@ class ProdInjRatioCalc(BlockInjIndex.BlockInjIndex):
                   / wafed_liqs.T.sum()
         self.block_prod_skin_table.index = self.block_prod_skin_table.index.droplevel()
         
+        
+    def block_skins_calc_kh_av(self, blocks_list_for_calc=None):
+      if blocks_list_for_calc==None:
+         blocks_list_for_calc = self.blocks_list
+      
+      self.block_inj_skin_table_kh_av = pd.DataFrame()
+      self.block_prod_skin_table_kh_av = pd.DataFrame()
+      for block in blocks_list_for_calc:
+         kh = self.kh_table_blocks[block]
+         prods = list((set(self.blocks_dict[block]) - set(self.injectors))&set(self.pi_table.columns))
+         injectors = list(set(self.blocks_dict[block]) & set(self.injectors))
+         #Pres
+         some_list = []
+         for i in prods:
+            if str(type(self.pres_table.get(i))) != "<type 'NoneType'>":
+               some_list.append( self.pres_table[i])
+         nPres = pd.DataFrame(some_list)
+         Pres = nPres.mean(skipna=True, axis=0)["Pres"]
+         #inj skin
+         beta_coef = -(pvt.PVTprops.p_avg_D_pat - np.log(2))
+         alpha = kh * pvt.PVTprops.krw_prime / pvt.PVTprops.mu_w / pvt.PVTprops.C
+         table = (self.bhp_table[injectors].T['avg_BhpInjTopPerfFaily']-Pres) / self.inj_table[injectors].T['InjRate']
+         block_inj_skins = pd.DataFrame(table.values*alpha, index=table.index, columns=table.columns) + beta_coef
+         #prod_skin
+         beta_coef = -(pvt.PVTprops.p_avg_D_well - np.log(2))
+         Prod_skins = ((1/self.pi_table[prods].T.copy())*alpha+beta_coef).T
+         Prod_skins.index = Prod_skins.index.droplevel()
+         #block Inj skin average
+         liq_rates = self.inj_table[injectors]
+         wafs = self.WAF_table_blocks["WAF"][block][injectors]
+         wafed_liqs = liq_rates * wafs
+         self.block_inj_skin_table_kh_av[block] = (block_inj_skins.T*wafed_liqs).T.sum() / wafed_liqs.T.sum()                 
+         self.block_inj_skin_table_kh_av[self.block_inj_skin_table>1000]=np.nan
+         self.block_inj_skin_table_kh_av.sort_index
+         #self.block_inj_skin_table_kh_av.index = self.block_inj_skin_table_kh_av.index.droplevel()
+         #block Prod skin average
+         skins = Prod_skins #self.prod_skin_table[prods]
+         deltaP = Pres - pvt.PVTprops.p_wf_P
+         #potential liq rates
+         liq_rates = pd.DataFrame((self.pi_table[prods].T.values * deltaP.values).T, \
+                        index = self.pi_table[prods].index.droplevel(), \
+                        columns=self.pi_table[prods].columns)
+         #self.Qliq_table[prods   ]
+         wafs = self.WAF_table_blocks["WAF"][block][prods]
+         wafed_liqs = liq_rates * wafs
+         self.block_prod_skin_table_kh_av[block] = \
+         pd.DataFrame(skins.values*wafed_liqs.values, \
+         index=wafed_liqs.index, columns=wafed_liqs.columns).T.sum()  \
+                     / wafed_liqs.T.sum()
+    
+     
+        
     def pi_ratio_calc(self, blocks_list_for_calc=None):
         """
         calculation of pseudo prodcutivivty injectivity ratio for block
@@ -66,12 +118,13 @@ class ProdInjRatioCalc(BlockInjIndex.BlockInjIndex):
         #self.pi_ratio_table = pd.DataFrame()
         self.block_inj_skin_calc(blocks_list_for_calc)
         self.block_prod_skin_calc(blocks_list_for_calc)
+        self.block_skins_calc_kh_av(blocks_list_for_calc)
         self.prod_inj_ratio_table = pd.DataFrame()
         if blocks_list_for_calc==None:
                 blocks_list_for_calc = self.blocks_list
         for block in blocks_list_for_calc:
-            i_skin = self.block_inj_skin_table[block]
-            p_skin = self.block_prod_skin_table[block]
+            i_skin = self.block_inj_skin_table_kh_av[block]
+            p_skin = self.block_prod_skin_table_kh_av[block]
             k = self.prod_inj_ratio_nominal[block]
             self.prod_inj_ratio_table[block] = k*(pvt.PVTprops.p_avg_D_pat + i_skin)/(pvt.PVTprops.p_avg_D_pat + p_skin)
         return                
@@ -80,11 +133,8 @@ if __name__=="__main__":
     """    NinetyDaysPresInputFileStr=".\input\\NinetyDaysPresInputFile.txt", \
     InjOfmFileStr=".\input\\InjOfmFile.txt", BlockMappingFileInput='.\input\\blocks_mapping.csv',\
     CellsMappingFileInput = '.\input\\cells_mapping.csv')"""
-    t = ProdInjRatioCalc()
+    t = ProdInjRatioCalc(".\\feb2017\\NinetyDaysPresInputFile2017.txt", ".\\feb2017\\InjOfmFile2017.txt", ".\\feb2017\\blocks_WS.csv", ".\\feb2017\\cells_WS.csv")
+    t.InterpFile = ".\\feb2017\\06 Petrophysical Evaluation results in RGTI form_.csv"
     t.load_data()
-    t.pi_ratio_calc()
-    SVA_blocks = filter(lambda t: t[:2]=="SV",t.block_inj_skin_table.columns)
-    WS_blocks = filter(lambda t: t[:2]=="WS", t.block_inj_skin_table.columns)
-    US_blocks = filter(lambda t: t[:2]=="US", t.block_inj_skin_table.columns)
-    #t.plot_list(["SVA-5"])
+    
     
